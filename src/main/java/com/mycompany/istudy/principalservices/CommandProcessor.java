f67915ee34a22cd2d2d155ad71279f37051929bc
chaambug <chaam12bug@yahoo.com>
2017-03-18 19:39:52 +0100

    Presentable version 1

--- src/main/java/com/mycompany/istudy/principalservices/CommandProcessor.java
+++ src/main/java/com/mycompany/istudy/principalservices/CommandProcessor.java
@@ -2,10 +2,6 @@ package com.mycompany.istudy.principalservices;
 
 import org.apache.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
  * Created by Varuni on 15.12.2016.
  */
@@ -16,17 +12,5 @@ public class CommandProcessor {
     public static void process(String command) throws Exception {
         //Create processor to run command
         Process process = Runtime.getRuntime().exec(command);
-    }
-
-    public static void processAndWrite(String command, String path) throws IOException {
-        //Create processor to run command
-        String[] commandd = {"cmd", "/c", command};
-        Process process = Runtime.getRuntime().exec(commandd);
-        System.out.println("the output stream is " + process.getOutputStream());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String s;
-        while ((s = reader.readLine()) != null) {
-            System.out.println(s);
-        }
     }
 }
